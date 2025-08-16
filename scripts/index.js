6259ca9b29946954b18e9585883e1b0711994856
KateB1598 <boikokate14@gmail.com>
2025-08-16 16:14:23 +0100

    HW18

---
+++ scripts/index.js
@@ -0,0 +1,23 @@
+console.log(
+  "JS #1. Домашнє завдання. Основи JavaScript: Працюємо зі змінними, типами даних"
+);
+let myNum = 10;
+let decimal2 = myNum.toFixed(2);
+console.log(decimal2);
+let myStr = "some string";
+console.log(myStr);
+console.log(myStr.toUpperCase());
+console.log(myStr);
+
+let myBool = true;
+console.log(myBool);
+
+let myArr = [1, 2, 3, 4, 5];
+console.log(myArr);
+
+let myObj = { first: "First Name", last: "Last Name" };
+console.log(myObj);
+
+let myBigInt = 123n;
+myBigInt = myBigInt + 1n;
+console.log(myBigInt);
