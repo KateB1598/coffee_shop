e413b605212eb8ac7488e95ff28252eb3c6deb79
KateB1598 <boikokate14@gmail.com>
2025-08-16 19:26:26 +0100

    Renew HW18

--- scripts/index.js
+++ scripts/index.js
@@ -4,18 +4,18 @@ console.log(
 let myNum = 10;
 let decimal2 = myNum.toFixed(2);
 console.log(decimal2);
-let myStr = "some string";
+const myStr = "some string";
 console.log(myStr);
 console.log(myStr.toUpperCase());
 console.log(myStr);
 
-let myBool = true;
+const myBool = true;
 console.log(myBool);
 
-let myArr = [1, 2, 3, 4, 5];
+const myArr = [1, 2, 3, 4, 5];
 console.log(myArr);
 
-let myObj = { first: "First Name", last: "Last Name" };
+const myObj = { first: "First Name", last: "Last Name" };
 console.log(myObj);
 
 let myBigInt = 123n;
