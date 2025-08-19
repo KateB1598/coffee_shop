9518a38f7a5df88f41773f013d13ed415545cd2c
KateB1598 <boikokate14@gmail.com>
2025-08-19 20:27:37 +0100

    HW19

--- scripts/index.js
+++ scripts/index.js
@@ -21,3 +21,27 @@ console.log(myObj);
 let myBigInt = 123n;
 myBigInt = myBigInt + 1n;
 console.log(myBigInt);
+
+/* HW 19*/
+console.log(
+  "JS #2. Домашнє завдання. Занурення в JavaScript: Від простих до складних обчислень і рядків"
+);
+
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+console.log(getRandomInt(1, 10));
+console.log(getRandomInt(40, 50));
+console.log(getRandomInt(1, 100));
+
+function greetByName(msg, name) {
+  return msg + ", " + name;
+}
+console.log(greetByName("Hi", "John"));
+console.log(greetByName("Hey", "Bob"));
+console.log(greetByName("Hello", "Mary"));
+
+function sumBigIntegers(numStr1, numStr2) {
+  return BigInt(numStr1) + BigInt(numStr2);
+}
+console.log(sumBigIntegers("9007199254740991", "9007199254740991"));
